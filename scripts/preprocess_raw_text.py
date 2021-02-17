4d3b52f9124c09d2e1831b8a2c163a751b3d2535
alexanderbaranof <alexanderbaranof@gmail.com>
2021-02-17 18:45:10 +0300

    feat: edit script for src file and added requirements

--- scripts/preprocess_raw_text.py
+++ scripts/preprocess_raw_text.py
@@ -4,9 +4,12 @@ import re
 from unidecode import unidecode
 from tqdm import tqdm
 
-nlp = spacy.load('en_core_web_sm', disable=['tagger', 'parser', 'ner'])
-for name in ('sentencizer', 'tagger'):
-    nlp.add_pipe(nlp.create_pipe(name))
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+sentencizer = nlp.create_pipe('sentencizer')
+nlp.add_pipe(sentencizer, first=True)
+
+print(nlp.pipe_names)
+print(nlp.pipeline)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -37,13 +40,17 @@ def sentence_iter(file_):
 
 def main(args):
     count = 0
+    print ("hello")
+    #lines = sum(1 for _ in sentence_iter(args.input))
+    #print (lines)
     with open(args.output, 'w') as fout, open(args.input, 'r') as fin:
-        for s in tqdm(sentence_iter(args.input)):
-            s = line.strip().split()
+        for s in tqdm(sentence_iter(args.input), total=40274446):
+            # s = line.strip().split()
             if len(s) >= args.min_len and len(s) <= args.max_len:
-                l = ['{}|{}'.format(token.text, token.pos_) for token in s]
+                l = ['{}|{}|{}'.format(token.text, token.lemma_, token.pos_) for token in s]
                 fout.write(' '.join(l) + '\n')
                 count += 1
+    print (count)
 
 if __name__ == '__main__':
     args = parse_args()
